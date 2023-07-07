17d349b925291ba597e2bba806a87b92b25b9cd7
Caitlin Wall <caitlinwall96@gmail.com>
2023-07-07 14:55:51 -0400

    Remove hyphens from pokemon names when used for data information; capitalize the names

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 const Card = ({ id, name }) => {
+  let nameWOHyphen = name;
+  if (name !== "ho-oh") {
+    nameWOHyphen = name.split('-')[0];
+  }
+
   return (
     <div className='tc bg-light-gray dib br3 pa3 ma2 grow bw2 shadow-5 pointer'>
       <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} alt={name}/>
       <div>
-        <h2>{name}</h2>
+        <h4>{(nameWOHyphen.charAt(0).toUpperCase() + nameWOHyphen.slice(1))}</h4>
       </div>
     </div>
   );
